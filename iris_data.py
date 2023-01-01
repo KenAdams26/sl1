c661540502491fd2a1d81000968358af9d3c79bd
KenAdams26 <121540748+KenAdams26@users.noreply.github.com>
2023-01-01 22:12:10 +0530

    Update iris_data.py

--- iris_data.py
+++ iris_data.py
@@ -1,7 +1,6 @@
-# Write a Python program to prepare Scatter Plot for Iris Dataset
-
+#1.Scatter Plot for Iris Dataset
 import pandas as pd
-iris=pd.read_csv("iris.csv")
-iris.plot(kind ="scatter" ,
-x='SepalLengthCm',
-y ='PetalLengthCm')
+Iris=pd.read_csv("Iris.csv")
+Iris.plot(kind='scatter',
+          x='SepalLengthCm',
+          y='SepalWidthCm')
